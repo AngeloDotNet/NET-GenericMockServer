489ee4c224b8878d0646178bc9d68816f5b766d8
Angelo Pirola <angelo9871@gmail.com>
2026-02-08 10:00:00 +0100

    Add entity fakers for seeding with Bogus library
    Added Customer, Order, Product, and Tag fakers implementing IEntityFaker<T> for realistic data generation. Introduced DefaultEntityFaker<T> for generic entity seeding using reflection. These changes enhance data seeding and testing capabilities in NETMockServer.

---
+++ src/NETMockServer/Seed/Fakers/OrderFaker.cs
@@ -0,0 +1,23 @@
+﻿using Bogus;
+using NETMockServer.Entities;
+using NETMockServer.Seed.Interfaces;
+
+namespace NETMockServer.Seed.Fakers;
+
+public class OrderFaker : IEntityFaker<Order>
+{
+    private readonly Faker<Order> faker;
+
+    public OrderFaker()
+    {
+        faker = new Faker<Order>()
+            .RuleFor(o => o.CreatedAt, f => f.Date.Recent(60))
+            .RuleFor(o => o.Items, f =>
+            {
+                // Items will be assigned later in seeder where product ids are known.
+                return [];
+            });
+    }
+
+    public Order Generate() => faker.Generate();
+}
---
+++ src/NETMockServer/Seed/Fakers/TagFaker.cs
@@ -0,0 +1,13 @@
+﻿using Bogus;
+using NETMockServer.Entities;
+using NETMockServer.Seed.Interfaces;
+
+namespace NETMockServer.Seed.Fakers;
+
+public class TagFaker : IEntityFaker<Tag>
+{
+    private readonly Faker<Tag> faker = new Faker<Tag>()
+        .RuleFor(t => t.Name, f => f.Lorem.Word());
+
+    public Tag Generate() => faker.Generate();
+}
