601098240d46059f30bc5e5ba00799c66487a3df
Angelo Pirola <angelo9871@gmail.com>
2026-02-08 10:00:34 +0100

    Refactor repository to use long IDs instead of Guid
    Updated repository interface and implementation to use long for entity IDs. Refactored GetByIdAsync, UpdateAsync, and DeleteAsync methods and helper logic. Improved property update logic in UpdateAsync. Cleaned up using statements and namespaces.

--- src/NETMockServer/Repositories/Interfaces/IGenericRepository.cs
+++ src/NETMockServer/Repositories/Interfaces/IGenericRepository.cs
@@ -1,10 +1,10 @@
-﻿namespace NETMockServer.Repositories.Interfaces;
+namespace NETMockServer.Repositories.Interfaces;
 
 public interface IGenericRepository<T> where T : class
 {
     Task<IEnumerable<T>> GetAllAsync(int skip = 0, int take = 100);
-    Task<T?> GetByIdAsync(Guid id);
+    Task<T?> GetByIdAsync(long id);
     Task<T> AddAsync(T entity);
-    Task<T?> UpdateAsync(Guid id, T entity);
-    Task<bool> DeleteAsync(Guid id);
+    Task<T?> UpdateAsync(long id, T entity);
+    Task<bool> DeleteAsync(long id);
 }
